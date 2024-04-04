a75fe1a8bb39bb2f1d938c7fd45d659a2dfcc202
Aatos Otronen <127504684+mildey22@users.noreply.github.com>
2024-04-04 12:49:01 +0300

    Moved controllers to web folder -Aatos

---
+++ src/main/java/com/tiimi1/petshop/web/ManufacturerController.java
@@ -0,0 +1,44 @@
+package com.tiimi1.petshop.web;
+
+import com.tiimi1.petshop.model.Product;
+import com.tiimi1.petshop.model.ProductRepository;
+
+import java.util.Locale.Category;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import com.tiimi1.petshop.model.Manufacturer;
+import com.tiimi1.petshop.model.ManufacturerRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ManufacturerController {
+
+    @Autowired
+    private ProductRepository prodRepo;
+    @Autowired
+    private ManufacturerRepository manuRepo;
+
+    @RequestMapping(value = { "/manufacturers" }, method = RequestMethod.GET)
+    public String showManufacturers(Model model) {
+        model.addAttribute("manufacturers", manuRepo.findAll());
+        return "manufacturers";
+    }
+
+    @RequestMapping("/addmanufacturer")
+    public String addManufacturer(Model model) {
+        model.addAttribute("manufacturer", new Manufacturer());
+        return "addmanufacturer";
+    }
+
+    @RequestMapping(value = "/savemanufacturer", method = RequestMethod.POST)
+    public String save(Manufacturer manufacturer) {
+        manuRepo.save(manufacturer);
+        return "redirect:manufacturers";
+    }
+
+}
---
+++ src/main/java/com/tiimi1/petshop/web/ProductController.java
@@ -0,0 +1,44 @@
+package com.tiimi1.petshop.web;
+
+import com.tiimi1.petshop.model.Product;
+import com.tiimi1.petshop.model.ProductRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import com.tiimi1.petshop.model.ManufacturerRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ProductController {
+
+    @Autowired
+    private ProductRepository prodRepo;
+    @Autowired
+    private ManufacturerRepository manuRepo;
+
+    @RequestMapping(value = { "/", "/products" }, method = RequestMethod.GET)
+    public String showProducts(Model model) {
+        model.addAttribute("products", prodRepo.findAll());
+        return "products";
+    }
+
+    @RequestMapping("/addproduct")
+    public String addProduct(Model model) {
+        model.addAttribute("product", new Product());
+        model.addAttribute("manufacturers", manuRepo.findAll());
+        return "addproduct";
+    }
+
+    @RequestMapping(value = "/saveproduct", method = RequestMethod.POST)
+    public String save(Product product) {
+        prodRepo.save(product);
+        return "redirect:products";
+    }
+
+
+
+}
