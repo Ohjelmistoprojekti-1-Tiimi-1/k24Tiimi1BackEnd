a5ae38dc2ad2166b0423afd0614a07ed31fa2e0a
pinkkila <petteri.pinkkila@outlook.com>
2024-04-04 16:30:26 +0300

    Changed CommandLineRunner from bean to implement PetshopApplicatioin #11

--- src/main/java/com/tiimi1/petshop/PetshopApplication.java
+++ src/main/java/com/tiimi1/petshop/PetshopApplication.java
@@ -5,38 +5,45 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 import com.tiimi1.petshop.model.Manufacturer;
 import com.tiimi1.petshop.model.ManufacturerRepository;
 import com.tiimi1.petshop.model.Product;
 import com.tiimi1.petshop.model.ProductRepository;
 
+
 @SpringBootApplication
-public class PetshopApplication {
+public class PetshopApplication implements CommandLineRunner {
 	private static final Logger log = LoggerFactory.getLogger(PetshopApplication.class);
+	private final ProductRepository productRepository;
+	private final ManufacturerRepository manufacturerRepository;
+
+	public PetshopApplication(ProductRepository productRepository, ManufacturerRepository manufacturerRepository) {
+		this.productRepository = productRepository;
+		this.manufacturerRepository = manufacturerRepository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PetshopApplication.class, args);
+		log.info("Application started");
 	}
 
-	@Bean
-	public CommandLineRunner productDemo(ProductRepository prodRepo, ManufacturerRepository manuRepo) {
-		return (args) -> {
-			log.info("couple demo manufacturers");
-			Manufacturer manufacturer1 = new Manufacturer("Doggy Stuff");
-			Manufacturer manufacturer2 = new Manufacturer("Catty Stuff");
-			Manufacturer manufacturer3 = new Manufacturer("Fishy Stuff");
 
-			manuRepo.save(manufacturer1);
-			manuRepo.save(manufacturer2);
-			manuRepo.save(manufacturer3);
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("couple demo manufacturers");
+		Manufacturer manufacturer1 = new Manufacturer("Doggy Stuff");
+		Manufacturer manufacturer2 = new Manufacturer("Catty Stuff");
+		Manufacturer manufacturer3 = new Manufacturer("Fishy Stuff");
 
-			log.info("couple demo products");
-			prodRepo.save(new Product("Dog booties", "Apparel", "Green", "Medium", 59.95, manufacturer1));
-			prodRepo.save(new Product("Cat Collar", "Apparel", "Red", "Small", 20.00, manufacturer2));
-			prodRepo.save(new Product("Fish Tank", "Living Space", "Black", "Large", 500.50, manufacturer3));
-		};
+		manufacturerRepository.save(manufacturer1);
+		manufacturerRepository.save(manufacturer2);
+		manufacturerRepository.save(manufacturer3);
+
+		log.info("couple demo products");
+		productRepository.save(new Product("Dog booties", "Apparel", "Green", "Medium", 59.95, manufacturer1));
+		productRepository.save(new Product("Cat Collar", "Apparel", "Red", "Small", 20.00, manufacturer2));
+		productRepository.save(new Product("Fish Tank", "Living Space", "Black", "Large", 500.50, manufacturer3));
 	}
 
 }
