9f7ad25d36ad1ef12f3c6251b3caa2d959891b75
pinkkila <petteri.pinkkila@outlook.com>
2024-04-04 15:42:54 +0300

    Removed unused imports from ManufacturerController and ProductController #2

--- src/main/java/com/tiimi1/petshop/web/ManufacturerController.java
+++ src/main/java/com/tiimi1/petshop/web/ManufacturerController.java
@@ -1,25 +1,17 @@
 package com.tiimi1.petshop.web;
 
-import com.tiimi1.petshop.model.Product;
-import com.tiimi1.petshop.model.ProductRepository;
-
-import java.util.Locale.Category;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-
 import com.tiimi1.petshop.model.Manufacturer;
 import com.tiimi1.petshop.model.ManufacturerRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class ManufacturerController {
 
-    @Autowired
-    private ProductRepository prodRepo;
     @Autowired
     private ManufacturerRepository manuRepo;
 
