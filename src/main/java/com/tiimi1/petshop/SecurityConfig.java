c2c727bf8a6deff159a73ef4d353a614d5d10642
pinkkila <petteri.pinkkila@outlook.com>
2024-04-25 00:25:52 +0300

    Added customerlogin endpoint and configuration for customer login #50

--- src/main/java/com/tiimi1/petshop/SecurityConfig.java
+++ src/main/java/com/tiimi1/petshop/SecurityConfig.java
@@ -4,14 +4,17 @@ import java.util.Arrays;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -46,12 +49,13 @@ public class SecurityConfig {
 
     // to disables security use:  .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests.anyRequest().permitAll());
     @Bean
+    @Order(1)
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf((csrf) -> csrf.disable()).cors(withDefaults())
+                .securityMatcher("/admin/**", "/login/**", "/logout")
                 .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
                         .requestMatchers("/")
                         .permitAll()
-                        .requestMatchers("/api/**").hasRole("USER")
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest()
                         .authenticated())
@@ -61,6 +65,21 @@ public class SecurityConfig {
                         .permitAll())
                 .logout(logout -> logout
                         .permitAll());
+        return http.build();
+    }
+
+    @Bean
+    @Order(2)
+    SecurityFilterChain filterChainRest(HttpSecurity http) throws Exception {
+        http.csrf((csrf) -> csrf.disable()).cors(withDefaults())
+                .securityMatcher("/api/**", "/signup/**", "/logincustomer")
+                .sessionManagement(
+                        (sessionManagement) -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+                        .requestMatchers("/", "/signup", "/logincustomer")
+                        .permitAll()
+                        .requestMatchers("/api/**").hasRole("USER")
+                        .anyRequest().authenticated());
         return http.build();
     }
 
--- src/main/java/com/tiimi1/petshop/web/LoginController.java
+++ src/main/java/com/tiimi1/petshop/web/LoginController.java
@@ -1,12 +1,19 @@
 package com.tiimi1.petshop.web;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 
+import com.tiimi1.petshop.model.AccountCredentials;
 import com.tiimi1.petshop.model.Customer;
 import com.tiimi1.petshop.model.CustomerRepository;
 
@@ -15,9 +22,11 @@ import com.tiimi1.petshop.model.CustomerRepository;
 public class LoginController {
     private final CustomerRepository customerRepository;
     private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(10);
+    private final AuthenticationManager authenticationManager;
 
-    public LoginController(CustomerRepository customerRepository) {
+    public LoginController(CustomerRepository customerRepository, AuthenticationManager authenticationManager) {
         this.customerRepository = customerRepository;
+        this.authenticationManager = authenticationManager;
     }
     
     @GetMapping({ "/", "/login" })
@@ -30,6 +39,7 @@ public class LoginController {
         return "home";
     }
 
+    // Customer signup
     @PostMapping("/signup")
     public ResponseEntity<?> singUpCustomer (@RequestBody Customer signUpCustomer) {
         if (customerRepository.existsByUsername(signUpCustomer.getUsername())) {
@@ -39,6 +49,19 @@ public class LoginController {
         Customer newCustomer = new Customer(signUpCustomer.getUsername(), encoder.encode(signUpCustomer.getPassword()), "USER");
         customerRepository.save(newCustomer);
         return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+    }
+
+    // Customer login
+    @PostMapping("/customerlogin")
+    public ResponseEntity<?> loginCustomer(@RequestBody AccountCredentials credentials) {
+        UsernamePasswordAuthenticationToken creds = new UsernamePasswordAuthenticationToken(credentials.username(), credentials.password());
+        Authentication auth = authenticationManager.authenticate(creds);
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+        context.setAuthentication(auth);
+        SecurityContextHolder.setContext(context);
+        return ResponseEntity.ok().header(HttpHeaders.AUTHORIZATION, "Authentication header")
+                .header(HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS,"Authorization")
+                .build();
     }
 
 }
