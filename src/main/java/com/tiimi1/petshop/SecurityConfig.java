9bf43c66fc9d7598825b65e79f7c34d5da1bdca2
pinkkila <petteri.pinkkila@outlook.com>
2024-04-25 13:19:21 +0300

    modified requestMatcher to allow products/** and manu/**

--- src/main/java/com/tiimi1/petshop/SecurityConfig.java
+++ src/main/java/com/tiimi1/petshop/SecurityConfig.java
@@ -16,7 +16,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -85,7 +84,7 @@ public class SecurityConfig {
                     (sessionManagement) -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
                     .requestMatchers(HttpMethod.POST, "/signup").permitAll()
-                    .requestMatchers(HttpMethod.GET, "/api/products", "/api/manufacturers").permitAll()
+                    .requestMatchers(HttpMethod.GET, "/api/products/**", "/api/manufacturers/**").permitAll()
                     .requestMatchers(HttpMethod.POST, "/customerlogin").permitAll()
                     .requestMatchers("/api/customer").permitAll().anyRequest().authenticated())
                     .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class)
