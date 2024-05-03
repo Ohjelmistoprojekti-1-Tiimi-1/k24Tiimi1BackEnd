b323e791b384202927b679ef2631e574ec301caf
majaluoma <olli.majaluoma@gmail.com>
2024-05-03 12:19:15 +0300

    cancellation returns products to database  #67

--- src/main/java/com/tiimi1/petshop/web/ReservationController.java
+++ src/main/java/com/tiimi1/petshop/web/ReservationController.java
@@ -1,24 +1,29 @@
 package com.tiimi1.petshop.web;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.ui.Model;
 
 import com.tiimi1.petshop.model.ReservationRepository;
+import com.tiimi1.petshop.model.ProductRepository;
+import com.tiimi1.petshop.model.Product;
 import com.tiimi1.petshop.model.Reservation;
+import com.tiimi1.petshop.model.ReservationProduct;
 
 
 @Controller
 public class ReservationController {
     private final ReservationRepository reservationRepository;
+    private final ProductRepository productRepository;
 
-    public ReservationController(ReservationRepository reservationRepository) {
+    public ReservationController(ReservationRepository reservationRepository, ProductRepository productRepository) {
         this.reservationRepository = reservationRepository;
+        this.productRepository = productRepository;
     }
 
     @GetMapping("/admin/reservations")
@@ -40,8 +45,20 @@ public class ReservationController {
     public String cancelReservation(@PathVariable("id") Long reservationId) {
         Objects.requireNonNull(reservationId);
         Reservation reservation = reservationRepository.findById(reservationId).get();
-        reservation.setCancelled(new Date(System.currentTimeMillis()));
-        reservationRepository.save(reservation);
+        if (reservation.getCancelled() != null) {
+            reservation.setCancelled(new Date(System.currentTimeMillis()));
+            List<ReservationProduct>  products = reservation.getReservationProducts();
+            
+            for (ReservationProduct reservationproduct : products) 
+            {
+                Product product = reservationproduct.getProduct();
+    
+                product.setInStock(product.getInStock() - reservationproduct.getCount());
+                productRepository.save(product);
+            }
+           
+            reservationRepository.save(reservation);
+        }
         return "redirect:/admin/reservations";
     }
 }
